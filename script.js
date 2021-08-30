bd51a7ef7225693b4a3d35cabe75dd207ce80059
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2021-08-30 12:30:37 +0300

    init: start swiper

---
+++ script.js
@@ -0,0 +1,11 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
+
+// init Swiper:
+const swiper = new Swiper('.swiper');
